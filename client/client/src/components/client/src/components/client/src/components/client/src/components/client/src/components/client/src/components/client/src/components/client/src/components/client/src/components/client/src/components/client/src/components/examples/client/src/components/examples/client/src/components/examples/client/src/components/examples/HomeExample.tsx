a45483fd1bde161b6c6456c4baff412059a52779
Chiranjjeevi Sivakumar <chiranjjeeve2017@gmail.com>
2025-11-30 12:13:11 +0530

    Create HomeExample.tsx

---
+++ client/client/src/components/client/src/components/client/src/components/client/src/components/client/src/components/client/src/components/client/src/components/client/src/components/client/src/components/client/src/components/client/src/components/examples/client/src/components/examples/client/src/components/examples/client/src/components/examples/HomeExample.tsx
@@ -0,0 +1,9 @@
+import Home from "@/pages/Home";
+
+export default function HomeExample() {
+  return (
+    <div className="w-full h-[700px] overflow-hidden rounded-lg">
+      <Home />
+    </div>
+  );
+}
